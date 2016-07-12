efa06811f4a284a3a786ca2fb1fd983e372e8973
qiyingying <qiyingying@boco.com.cn>
2016-07-12 18:06:06 +0800

    添加用户实体

---
+++ boom-domain/src/main/java/com/fcst/boom/domain/User.java
@@ -0,0 +1,140 @@
+package com.fcst.boom.domain;
+
+import java.io.Serializable;
+
+public class User extends BaseEntity implements Serializable {
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -1660394325814677848L;
+
+	private String loginName;//用户登录名
+	
+	private String password;//密码
+	
+	private String birthday;//出生年月
+	
+	private String name;//姓名
+	
+	private String sex;//性别 1为男  0为女
+	
+	private String email;//email
+	
+	private String phone;
+	
+	private String mobile;
+	
+	private String login_ip;
+	
+	private String lastDate;//最后登录时间
+	
+	private String state;// 1为在用  0 为不在用
+	
+	private String photoUrl;//照片路径
+	
+	
+
+	public String getBirthday() {
+		return birthday;
+	}
+
+	public void setBirthday(String birthday) {
+		this.birthday = birthday;
+	}
+
+	
+
+	public String getLoginName() {
+		return loginName;
+	}
+
+	public void setLoginName(String loginName) {
+		this.loginName = loginName;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public String getMobile() {
+		return mobile;
+	}
+
+	public void setMobile(String mobile) {
+		this.mobile = mobile;
+	}
+
+	public String getLogin_ip() {
+		return login_ip;
+	}
+
+	public void setLogin_ip(String login_ip) {
+		this.login_ip = login_ip;
+	}
+
+	public String getLastDate() {
+		return lastDate;
+	}
+
+	public void setLastDate(String lastDate) {
+		this.lastDate = lastDate;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	public String getSex() {
+		return sex;
+	}
+
+	public void setSex(String sex) {
+		this.sex = sex;
+	}
+
+	public String getPhotoUrl() {
+		return photoUrl;
+	}
+
+	public void setPhotoUrl(String photoUrl) {
+		this.photoUrl = photoUrl;
+	}
+	
+	
+	
+	
+
+}
