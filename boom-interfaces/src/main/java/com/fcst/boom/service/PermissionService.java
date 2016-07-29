3b74d3f6a3350f6ba621826eb0b8cd5d8d34317c
qiyingying <qiyingying@boco.com.cn>
2016-07-29 10:58:26 +0800

    s

---
+++ boom-interfaces/src/main/java/com/fcst/boom/service/PermissionService.java
@@ -0,0 +1,20 @@
+package com.fcst.boom.service;
+
+import java.util.List;
+
+import com.fcst.boom.domain.Permission;
+
+/**
+ * Created by codegeneratorplugin
+ * User: yankun
+ * Date: 2016年03月04日
+ * Time: 23:06:12
+ * 功能:T_ROOM表Service接口
+ */
+
+public interface PermissionService {
+
+	List<Permission> getAllPermission(Long roleId);
+
+ 
+}
