ae1cf8efb66df7a5f7ca04f2870c4d1351507f2b
qiyingying <qiyingying@boco.com.cn>
2016-07-29 10:59:06 +0800

    s

--- boom-domain/src/main/java/com/fcst/boom/domain/Role.java
+++ boom-domain/src/main/java/com/fcst/boom/domain/Role.java
@@ -8,10 +8,17 @@ public class Role extends BaseEntity implements Serializable {
 	 * 
 	 */
 	private static final long serialVersionUID = 7336533771042458641L;
+	private String id;      // 角色ID
 	private String name; 	// 角色名称
 	private String enname;	// 英文名称
 	
 	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
 	public String getName() {
 		return name;
 	}
