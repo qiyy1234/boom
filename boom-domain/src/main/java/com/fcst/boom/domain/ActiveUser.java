446f8382a8f686b1bcfe8fcfeffdf0093662bd16
qiyingying <qiyingying@boco.com.cn>
2016-09-19 10:56:02 +0800

    acti

---
+++ boom-domain/src/main/java/com/fcst/boom/domain/ActiveUser.java
@@ -0,0 +1,57 @@
+package com.fcst.boom.domain;
+
+import java.io.Serializable;
+import java.util.List;
+
+public class ActiveUser extends BaseEntity  implements Serializable{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	private String userid;
+	private String usercode;//账号
+	private String name;//用户名称
+	private String sceneid;
+	private List<Permission> menus;//菜单
+	private List<Permission> permissions;//权限
+		
+	public String getSceneid() {
+		return sceneid;
+	}
+	public void setSceneid(String sceneid) {
+		this.sceneid = sceneid;
+	}
+	public String getUserid() {
+		return userid;
+	}
+	public void setUserid(String userid) {
+		this.userid = userid;
+	}
+	public String getUsercode() {
+		return usercode;
+	}
+	public void setUsercode(String usercode) {
+		this.usercode = usercode;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public List<Permission> getMenus() {
+		return menus;
+	}
+	public void setMenus(List<Permission> menus) {
+		this.menus = menus;
+	}
+	public List<Permission> getPermissions() {
+		return permissions;
+	}
+	public void setPermissions(List<Permission> permissions) {
+		this.permissions = permissions;
+	}
+	
+	
+}
